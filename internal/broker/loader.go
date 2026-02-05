227ea17108f10b01fb664bc2f9e42cd7c3e576d1
anshikakalpana <anshikajain196872@gmail.com>
2026-02-05 23:28:46 +0530

    validate broker config and apply default values

--- internal/broker/loader.go
+++ internal/broker/loader.go
@@ -1,12 +1,3 @@
-package broker
-
-import (
-	"fmt"
-	"os"
-
-	"gopkg.in/yaml.v3"
-)
-
 func LoadBrokerConfig(path string) (*BrokerConfig, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
@@ -19,19 +10,72 @@ func LoadBrokerConfig(path string) (*BrokerConfig, error) {
 	}
 
 	
+
 	if cfg.Broker.Service.Name == "" {
 		return nil, fmt.Errorf("broker.service.name is required")
 	}
+
+
+	
 	if cfg.Broker.Service.DataDir == "" {
 		return nil, fmt.Errorf("broker.service.dataDir is required")
 	}
-	if cfg.Broker.Service.Host == "" {
-		return nil, fmt.Errorf("broker.service.host is required")
-	}
-	if cfg.Broker.Service.Port == 0 {
-		return nil, fmt.Errorf("broker.service.port is required")
-	}
+
 	
+	if cfg.Broker.Service.Host == "" {
+		cfg.Broker.Service.Host = "localhost"
+	}
+
+
+	if cfg.Broker.Service.Port == 0 {
+		cfg.Broker.Service.Port = 8080
+	}
+
+
+
+	if cfg.Segment.MaxMessageBytes == 0 {
+		cfg.Segment.MaxMessageBytes = 1 * 1024 * 1024 // 1MB
+	}
+
+
+
+	if cfg.Segment.MaxSegmentBytes == 0 {
+		cfg.Segment.MaxSegmentBytes = 1 * 1024 * 1024 * 1024 // 1GB
+	}
+
+
+
+	if cfg.Segment.FlushIntervalMs == 0 {
+		cfg.Segment.FlushIntervalMs = 1000
+	}
+
+
+
+	if cfg.Segment.FlushMessages == 0 {
+		cfg.Segment.FlushMessages = 1000
+	}
+
+
+
+	if cfg.Retention.MaxAgeSeconds == 0 {
+		cfg.Retention.MaxAgeSeconds = 7 * 24 * 60 * 60 // 7 days
+	}
+
+
+
+	if cfg.Retention.MaxBytes == 0 {
+		cfg.Retention.MaxBytes = 1 * 1024 * 1024 * 1024 // 1GB
+	}
+
+
+
+	if cfg.Retention.CheckIntervalMs == 0 {
+		cfg.Retention.CheckIntervalMs = 1000
+	}
+
+
 
 	return &cfg, nil
+
+
 }
